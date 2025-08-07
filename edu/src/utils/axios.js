25f82a556e896375eafe60f176d9000e4233b128
Anjan-thedestroyer <paudelabinash58@gmail.com>
2025-08-07 11:41:32 +0545

    Initial commit

--- edu/src/utils/axios.js
+++ edu/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://gharbeti-sewa.onrender.com/api',
     timeout: 10000,
     withCredentials: true,
     headers: {
